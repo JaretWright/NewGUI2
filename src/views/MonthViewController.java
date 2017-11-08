74d5910ecd071c02642e6feb4520db0f296f6fd3
jaret_000 <jaret_000@192.168.0.30>
2017-11-08 07:59:56 -0500

    Added previous year data series to graph

--- src/views/MonthViewController.java
+++ src/views/MonthViewController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package views;
 
 import java.net.URL;
@@ -11,11 +6,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -36,7 +33,9 @@ public class MonthViewController implements Initializable {
     @FXML
     private NumberAxis hoursAxis;
     
-    private XYChart.Series series;
+    private XYChart.Series currentYearSeries;
+    private XYChart.Series lastYearSeries;
+    
     
     
     /**
@@ -44,22 +43,55 @@ public class MonthViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         series = new XYChart.Series<>();
-         
-         //barChart.setTitle("Hours Worked");
-         //monthAxis.setLabel("Months");
-         hoursAxis.setLabel("Hours");
-         series.setName("Hours");
-         
+        currentYearSeries = new XYChart.Series<>();
+        lastYearSeries = new XYChart.Series<>();
+        
+        initializeSeries(lastYearSeries);
+        initializeSeries(currentYearSeries);
+
+        //barChart.setTitle("Hours Worked");
+        //monthAxis.setLabel("Months");
+        hoursAxis.setLabel("Hours");
+        currentYearSeries.setName("2017");
+        lastYearSeries.setName("2016");
+        
         try {
             populateChartFromDB();
         } catch (SQLException ex) {
             Logger.getLogger(MonthViewController.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        barChart.getData().addAll(series);
-         
+        displaySeriesContents(lastYearSeries);
+        
+        
+        barChart.getData().addAll(lastYearSeries);
+        barChart.getData().addAll(currentYearSeries);
     }    
+    
+    public void displaySeriesContents(XYChart.Series series)
+    {
+        for (Object data : series.getData())
+        {
+            System.out.println(data);
+        }
+    }
+    
+    public void initializeSeries(XYChart.Series series)
+    {
+       series.getData().add(new XYChart.Data("January", 0));
+       series.getData().add(new XYChart.Data("February", 0));
+       series.getData().add(new XYChart.Data("March", 0));
+       series.getData().add(new XYChart.Data("April", 0));
+       series.getData().add(new XYChart.Data("May", 0));
+       series.getData().add(new XYChart.Data("June", 0));
+       series.getData().add(new XYChart.Data("July", 0));
+       series.getData().add(new XYChart.Data("August", 0));
+       series.getData().add(new XYChart.Data("September", 0));
+       series.getData().add(new XYChart.Data("October", 0));
+       series.getData().add(new XYChart.Data("November", 0));
+       series.getData().add(new XYChart.Data("December", 0));
+    }
+    
     
     public void populateChartFromDB() throws SQLException
     {
@@ -76,9 +108,9 @@ public class MonthViewController implements Initializable {
             statement = conn.createStatement();
             
             //3.  create a string with sql statement
-            String sql = "SELECT MONTHNAME(dayWorked), SUM(hoursworked) "+
+            String sql = "SELECT YEAR(dayWorked), MONTHNAME(dayWorked), SUM(hoursworked) "+
                          "FROM hoursWorked "+
-                         "GROUP BY MONTH(dayWorked) "+
+                         "GROUP BY YEAR(dayWorked), MONTH(dayWorked) "+
                          "ORDER BY MONTH(dayWorked); ";
             
             //4. execute the query
@@ -86,7 +118,10 @@ public class MonthViewController implements Initializable {
 
             while (resultSet.next())
             {
-                series.getData().add(new XYChart.Data(resultSet.getString(1), resultSet.getInt(2)));
+                if (resultSet.getInt(1) == LocalDate.now().getYear())
+                    currentYearSeries.getData().add(new XYChart.Data(resultSet.getString(2), resultSet.getInt(3)));
+                else
+                    lastYearSeries.getData().add(new XYChart.Data(resultSet.getString(2), resultSet.getInt(3)));
             }      
         } 
         catch (Exception e)
